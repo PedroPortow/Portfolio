329b741b82954d4656c40c5c3a73b49631e66fd5
PedroPortow <pedrolportow@gmail.com>
2022-03-04 14:14:58 -0300

    segundo commit

--- src/components/HeroSection/HeroElements.js
+++ src/components/HeroSection/HeroElements.js
@@ -11,15 +11,12 @@ export const InfoContainer = styled.div`
 `
 
 export const InfoWrapper = styled.div`
-    display: grid;
-    z-index: 1;
+    display: flex ;
+    flex-wrap: wrap;
     height: 100vh;
     width: 100%;
-    max-width: 2100px;
-    margin-right: auto;
-    margin-left: auto;
-    padding: 0 24px;
     justify-content: center;
+    align-items:center ;
 `
 
 export const FogueteText = styled.p`
@@ -29,31 +26,21 @@ export const FogueteText = styled.p`
 `
 
 export const InfoRow = styled.div`
-    display: grid;
-    grid-auto-columns: minmax(auto, fr);
+    display: flex;
+    justify-content: center ;
     align-items: center;
-    grid-template-areas: ${({imgStart}) => (imgStart ? `'col2 col1'` : `'col1 col2'`)};
-
-    @media screen and (max-width: 768px){
-        grid-template-areas: ${({imgStart}) => (imgStart ? `'col1' 'col2'` : `'col1 col1' `)};
-    }
+    flex-wrap: wrap;
 
 `
 
 export const Column1 = styled.div`
-    margin-bottom: 15px;
-    padding: 0 15px;
-    grid-area: col1;
+
 `
 
 export const Column2 = styled.div`
-    margin-bottom: 15px;
-    padding: 0 15px;
-    grid-area: col2;
-    margin-left: 150px;
 
     @media screen and (max-width: 768px){
-        display: none;
+    
     }
 `
 
@@ -117,28 +104,24 @@ export const HeroInvite = styled(Link)`
 
     color: #FFFFFF;
     cursor: pointer;
-    flex-shrink: 0;
+   
     font-size: 16px;
     font-weight: 500;
     height: 3rem;
-   
+ 
     padding: 1rem;
-    text-align: center;
+    
     text-shadow: rgba(0, 0, 0, 0.25) 0 3px 8px;
     transition: all .5s;
     user-select: none;
     -webkit-user-select: none;
     touch-action: manipulation;
-    }
+    
+   
 
     &:hover {
     box-shadow: rgba(80, 63, 205, 0.1) 0 1px 30px;
     transition-duration: .1s;
-    }
-
-    @media (min-width: 768px) {
-    .button-36 {
-        padding: 0 2.6rem;
     }
 
 `
